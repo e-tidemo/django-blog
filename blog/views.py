50c03c14e06f0256acff8eb98fb9011ba5e66d6f
Elvira Tidemo <elvir@MacBook-Air.local>
2024-02-13 14:55:55 +0100

    Add excerpt to models.py

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def my_blog(request):
-    return HttpResponse("Hello, this is where I blog!")
+    return HttpResponse("Hello Blog!")
